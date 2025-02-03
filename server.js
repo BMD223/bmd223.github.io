87d80bc05b0e03c3e40f466281d69fd53db61aef
BBD223 <bartoszdziubaimportantstuff@gmail.com>
2025-02-03 18:40:06 +0100

    First building step- redefining the project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Database connection
+const db = require('./models/database');
+
+// Routes
+const mainRoutes = require('./routes/index');
+app.use('/', mainRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
